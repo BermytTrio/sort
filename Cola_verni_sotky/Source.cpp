40594bb3ec30d18cc26a123a7edf8e2e77e6c1c9
unknown <platonovartem46@gmail.com>
2021-11-23 17:51:50 +0200

    Implemant selection add insert sorting algorythm

--- Cola_verni_sotky/Source.cpp
+++ Cola_verni_sotky/Source.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 #include <ctime>
+#include <chrono>
 
 using namespace std;
 
 void printArr(int* arr, int size);
 int* initArr(int size);
 void bubbleSort(int* arr, int size);
+void selectionSort(int* arr, int size);
+void insertSort(int* arr, int size);
 
 int main() {
 	srand(time(NULL));
-	int size = 10;
+	int size = 100000;
 	int* arr = initArr(size);
-	printArr(arr,  size);
-	bubbleSort(arr, size);
-	printArr(arr, size);
+	//printArr(arr,  size);
+	auto t1 = std::chrono::high_resolution_clock::now();
+	//bubbleSort(arr, size);
+	//selectionSort(arr, size);
+	insertSort(arr, size);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+	std::cout << duration;
+	//printArr(arr, size);
 }
 
 void printArr(int* arr, int size)
+{
+}
+
+/*void printArr(int* arr, int size)
 {
 	for (int i = 0; i < size; i++) {
 		cout << arr[i] << " ";
 	}
 	cout << endl;
 }
-
+*/
 int* initArr(int size)
 {
 	int* arr = new int[size];
@@ -41,6 +54,37 @@ void bubbleSort(int* arr, int size)
 				swap(arr[j], arr[j + 1]);
 			}
 		}
+	}
+}
+
+void selectionSort(int* arr, int size)
+{
+
+	for (int i = 0; i < size - 1; i++) {
+		int tmp = arr[i];
+		int index = i;
+		for (int j = i; j < size; j++) {
+			if (tmp > arr[j]) {
+				tmp = arr[j];
+				index = j;
+			}
+		}
+		swap(arr[i], arr[index]);
+		//printArr(arr, size);
+	}
+}
+
+
+void insertSort(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++) {
+		int j = i + 1;
+		int tmp = arr[j];
+		for (; j >= 0 && tmp < arr[j - 1]; j--) {
+			arr[j] = arr[j - 1];
+		}
+		arr[j] = tmp;
+		//printArr(arr, size);
 	}
 }
 
